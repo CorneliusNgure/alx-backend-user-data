5dd97500e7fc3f7403b56141025243f06162fefc
CorneliusNgure <kingcornelius07@gmail.com>
2024-09-27 13:17:25 -0400

    Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -74,7 +74,7 @@ class DB:
                 raise InvalidRequestError(f"Invalid attribute: {key}")
 
         # Query the database to find the first user matching the filters
-        user = self._session.query(User).filter_by(**kwargs).first()
+        user = self._session.query(User).filter_by(**kwargs).one()
 
         if user is None:
             raise NoResultFound()
