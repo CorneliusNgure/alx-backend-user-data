a451affbe164650fe1f6c1e84b19811ca372862d
CorneliusNgure <kingcornelius07@gmail.com>
2024-09-28 08:41:44 -0400

    Login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -39,5 +39,34 @@ def register_user():
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route("/sessions", methods=["POST"])
+def login():
+    """
+    POST /sessions route to log in a user.
+
+    Expects:
+    - email: form data (str)
+    - password: form data (str)
+
+    Returns:
+    - JSON with email and message on successful login
+    - 401 status if login information is incorrect
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not email or not password:
+        abort(401)  # missing credentials
+
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        # set session ID as a cookie
+        response.set_cookie("session_id", session_id)
+        return response, 200
+    else:
+        abort(401)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
